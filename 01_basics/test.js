77d016c3a599ec962d5e075830c833ec6ebd4f3f
Tanu Srivastav <94117849+Tanu9452@users.noreply.github.com>
2023-12-26 15:15:03 +0000

    String practice

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,32 @@
 console.log("Rupali");
+console.log("Tanu")
+
+const name="Tanu"
+const Age="24"
+
+//string practice
+
+
+//console.log(name + Age +"Ayodhya");
+//console.log(`Hello my Name is ${name} and I am ${Age} year old`);
+
+//console.log(`Hello my Name is ${name.toUpperCase()} and I am ${Age} year old`);
+
+
+
+const myName= new String('Tanu Srivastava')
+
+    //    console.log(myName.__proto__);
+      //  console.log(myName[5]);
+        //console.log(myName.length)
+       // console.log(myName.toUpperCase())
+console.log(myName.charAt(7));          //to find the charecter at index
+console.log(myName.indexOf("T"))        //to find the index of charecter
+
+const MyString= myName.substring(0, 3)
+console.log(MyString);
+
+const myNewString=myName.slice(-9,12)
+console.log(myNewString);
+
+
